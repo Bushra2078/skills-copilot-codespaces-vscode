069ab665551656ba977fdd8f48f3f299f1e5caec
Bushra <bushra.inayat20@gmail.com>
2023-07-17 22:08:52 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,12 @@
+function  skillsMember() { 
+     return { 
+        restrict:'modules/skills/views/member.html',
+controllerAs:'vm',
+bindToController: true,
+scope:{
+member:'"'
+}
+};
+}
+        
+     
